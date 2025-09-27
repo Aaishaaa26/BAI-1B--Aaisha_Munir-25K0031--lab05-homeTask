2cbbb0d03dbad22a14474a093440eff619418b59
Aaisha Munir <k250031@nu.edu.pk>
2025-09-28 04:06:26 +0500

    Add files via upload

---
+++ q3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+	int a,b,c,discriminant;
+	printf("For determining discriminant of ax² + bx + c\nEnter a:");
+	scanf("%d",&a);
+	printf("Enter b:");
+	scanf("%d",&b);
+	printf("Enter c:");
+	scanf("%d",&c);
+	fflush(stdin);
+	discriminant = (b*b) - (4*a*c);
+	if (discriminant == 0){
+		printf("one real root\n");
+	}
+	else if (discriminant>0){
+		printf("two real roots\n");
+	}
+	else{ 
+		printf("No real roots, imaginary roots\n");
+	}
+	return 0;
+}
